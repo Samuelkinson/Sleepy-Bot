6fbc2656a114fb79c49ce953506fd056bdcf1c95
Samuelkinson <samuel.marques.15425@gmail.com>
2022-03-23 23:55:29 +0000

    Rewrite no código do C19, pois embed dava erro devido a "null"

--- Embeds/CommandEmbeds/UtilityEmbeds/Covid-19_Embeds/covid19.js
+++ Embeds/CommandEmbeds/UtilityEmbeds/Covid-19_Embeds/covid19.js
@@ -1,28 +1,26 @@
-module.exports = (Discord, msg, c, country, flag ) =>{
+function formatNumber(num) {
+    return num.toLocaleString().replace(/,/g, ' ')
+}
 
-    function formatNumber(num) {
-        return num.toLocaleString().replace(/,/g, ' ')
-    }
-    const embed = new Discord.MessageEmbed()
-	.setTitle(`${flag ? flag : "\:earth_americas:"} ${country}`)
-	.setDescription("Estatísticas do país")
-	.addFields(
-        {name: '🏥Total de casos', value: formatNumber(c.cases.total), inline: true},
-        {name: '😷Novos casos', value: formatNumber(c.cases.new), inline: true},
-        {name: '🤒Casos Ativos', value: formatNumber(c.cases.active), inline: true}, 
-		{name: '☢️Casos Criticos', value: formatNumber(c.cases.critical), inline: true},
-        {name: '😴Casos Recuperados', value: formatNumber(c.cases.recovered), inline: true},
+module.exports = (Discord, msg,  c, country, flag  ) =>{
+ 
+    let embed = new Discord.MessageEmbed()
+    .setTitle(`${flag ? flag : "\:earth_americas:"} ${country}`)
+	.setDescription("Estatísticas do país") 
+    .addFields(
+        {name: '🏥Total de casos', value: `${c.cases.total? formatNumber(c.cases.total) : 'Sem valor'} `, inline: true},
+        {name: '😷Novos casos', value: `${c.cases.new ? formatNumber(c.cases.new) : 'Sem valor'} ` , inline: true}, 
+        {name: '🤒Casos Ativos', value: `${c.cases.active ? formatNumber(c.cases.active) : 'Sem valor'} `, inline: true},
+        {name: '☢️Casos Criticos', value: `${c.cases.critical ? formatNumber(c.cases.critical) : 'Sem valor'} `, inline: true},
+        {name: '😴Casos Recuperados', value: `${c.cases.recovered ? formatNumber(c.cases.recovered) : 'Sem valor'} `, inline: true},
         )
-	.addFields(
-			{name: '☠️Total de Mortes', value: formatNumber(c.deaths.total), inline: true},
-			{name: '☠️Novas Mortes', value: formatNumber(c.deaths.new), inline: true},
-			{name: '🧪Total de Testes', value: formatNumber(c.tests.total), inline: true},
-		)			
+    .addFields(
+			{name: '☠️Total de Mortes', value: `${c.deaths.total? formatNumber(c.deaths.total) : 'Sem valor'} `, inline: true},
+			{name: '☠️Novas Mortes', value: `${c.deaths.new? formatNumber(c.deaths.new) : 'Sem valor'} `, inline: true},
+			{name: '🧪Total de Testes', value: `${c.tests.total? formatNumber(c.tests.total) : 'Sem valor'} `, inline: true},
+		)	
     .setFooter({
-        text:`Última atualização: ${new Date(c.time).toISOString().substring(0, 10)}; Comando Patrocinado por @SleepyBot 😴 `  
-    })
-        
-    
-
+            text:`Última atualização: ${new Date(c.time).toISOString().substring(0, 10)}; Comando Patrocinado por @SleepyBot 😴 `  
+        })
     return msg.channel.send({embeds:[embed]}).then(msg.delete()) 
 }
